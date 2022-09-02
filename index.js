cc0e3c9a93fe1321ee10b0436ae4af9f6854d920
Malvino-creator <malvin.opondo@student.moringaschool.com>
2022-09-02 11:56:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () =>{
     document.querySelector('p').innerHTML = "This is really cool!"
-})
+    })
